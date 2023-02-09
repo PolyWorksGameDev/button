392545c0c03d65bbf5ed68bc8893f6333c3e3413
PolyWorksGameDev <97145426+PolyWorksGameDev@users.noreply.github.com>
2023-02-08 19:40:27 -0500

    Add files via upload

---
+++ Button/Button/button.vb
@@ -0,0 +1,7 @@
+﻿Public Class button
+    Dim score As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        score = score + 1
+        Label1.Text = score
+    End Sub
+End Class
